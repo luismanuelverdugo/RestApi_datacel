8e42adcaa302a064fc827367193cede0ee4c4923
Luis Manuel Verdugo <43182044+luismanuelverdugo@users.noreply.github.com>
2024-12-03 21:01:11 -0700

    Add files via upload

--- RestApi.py
+++ RestApi.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from routes.rutas_clientes import rutaclientes
 from routes.rutas_productos import rutaproductos
+from routes.rutas_ventas import rutaventas
+from routes.rutas_dventas import rutadventas
 
 app=FastAPI()
 
 app.include_router(rutaclientes)
 app.include_router(rutaproductos)
+app.include_router(rutaventas)
+app.include_router(rutadventas)
